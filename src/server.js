22ca27e280406e841f77b7cd517b83ac90bb26f0
Oleg <luchkoo22@gmail.com>
2026-02-01 11:37:01 +0200

    task4

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 
@@ -11,6 +12,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 // Routers
 import notesRouters from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 // Використовуємо значення з .env або дефолтний порт 3000
@@ -18,9 +20,13 @@ const PORT = process.env.PORT ?? 3000;
 
 // Middleware
 app.use(logger);
-app.use(express.json());
+app.use(express.json({ limit: '5mb' })); // Розбір JSON з обмеженням розміру
 app.use(helmet()); // Додає безпекові заголовки
 app.use(cors()); // Дозволяє запити з будь-яких джерел
+app.use(cookieParser()); // Розбір cookie
+
+// Auth routers
+app.use(authRoutes);
 
 // Notes routers
 app.use(notesRouters);
